91307dbe395ef24d59458099317d6e51963e9279
xHlpNw <aleksashin990@gmail.com>
2025-07-14 15:02:06 +0400

    initialize commit

---
+++ src/main/java/com/example/production_practice/ProductionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.production_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionPracticeApplication.class, args);
+	}
+
+}
